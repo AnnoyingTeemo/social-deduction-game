57f6b49d0a35abb9a6b1844696f7c690e0b1e66e
ewok <mjbwighton@gmail.com>
2022-05-02 20:32:53 +1200

    pls work pls

--- I like cheese/Assets/Scripts/PlayerMovement.cs
+++ I like cheese/Assets/Scripts/PlayerMovement.cs
@@ -22,70 +22,78 @@ public class PlayerMovement : Photon.Bolt.EntityBehaviour<IPlayerState>
     // Start is called before the first frame update
     public override void Attached()
     {
-        oxygenText = GameObject.Find("Oxygen").GetComponent<Text>();
-
-        rgbdy = gameObject.GetComponent<Rigidbody>();
-        state.SetTransforms(state.PlayerTransform, gameObject.transform);
-        state.PlayerOxygen = oxygen;
         if (entity.IsOwner) {
-            playerCamera.gameObject.SetActive(true);
-        }
+            oxygenText = GameObject.Find("Oxygen").GetComponent<Text>();
 
-        InvokeRepeating("DrainOxygen", 1, 1);
+            rgbdy = gameObject.GetComponent<Rigidbody>();
+            state.SetTransforms(state.PlayerTransform, gameObject.transform);
+            state.PlayerOxygen = oxygen;
+            if (entity.IsOwner) {
+                playerCamera.gameObject.SetActive(true);
+            }
+
+            InvokeRepeating("DrainOxygen", 1, 1);
+        }
     }
 
     // Update is called once per frame
     public override void SimulateOwner()
     {
-        Vector3 movement = Vector3.zero;
+        if (entity.IsOwner) {
+            Vector3 movement = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.A)) {
-            //movement.x -= speed;
-            rgbdy.AddForce(-transform.right * speed);
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            //movement.x += speed;
-            rgbdy.AddForce(transform.right * speed);
-        }
-        if (Input.GetKey(KeyCode.W)) {
-            //movement.z += speed;
-            rgbdy.AddForce(transform.forward * speed);
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            //movement.z -= speed;
-            rgbdy.AddForce(-transform.forward * speed);
-        }
+            if (Input.GetKey(KeyCode.A)) {
+                //movement.x -= speed;
+                rgbdy.AddForce(-transform.right * speed);
+            }
+            if (Input.GetKey(KeyCode.D)) {
+                //movement.x += speed;
+                rgbdy.AddForce(transform.right * speed);
+            }
+            if (Input.GetKey(KeyCode.W)) {
+                //movement.z += speed;
+                rgbdy.AddForce(transform.forward * speed);
+            }
+            if (Input.GetKey(KeyCode.S)) {
+                //movement.z -= speed;
+                rgbdy.AddForce(-transform.forward * speed);
+            }
 
-        if (movement != Vector3.zero) {
-            //transform.position = transform.position + (movement.normalized * speed * BoltNetwork.FrameDeltaTime);
+            if (movement != Vector3.zero) {
+                //transform.position = transform.position + (movement.normalized * speed * BoltNetwork.FrameDeltaTime);
+            }
+
+            float h = camspeed * Input.GetAxis("Mouse X");
+            // float v = verticalSpeed * Input.GetAxis("Mouse Y");
+            transform.Rotate(0, h, 0);
+
+            //Let everyone know this players oxygen level
+            state.PlayerOxygen = oxygen;
+
+            oxygenText.GetComponent<Text>().text = "Oxygen: " + oxygen.ToString();
         }
-
-        float h = camspeed * Input.GetAxis("Mouse X");
-        // float v = verticalSpeed * Input.GetAxis("Mouse Y");
-        transform.Rotate(0, h, 0);
-
-        //Let everyone know this players oxygen level
-        state.PlayerOxygen = oxygen;
-
-        oxygenText.GetComponent<Text>().text = "Oxygen: " + oxygen.ToString();
     }
 
     public void DrainOxygen() {
-        oxygen -= 1;
+        if (entity.IsOwner) {
+            oxygen -= 1;
 
-        if (oxygen < 0) {
-            oxygen = 0;
+            if (oxygen < 0) {
+                oxygen = 0;
+            }
+
+            state.PlayerOxygen = oxygen;
         }
-
-        state.PlayerOxygen = oxygen;
     }
 
     public void GainOxygen(int amount) {
-        oxygen += amount;
-        if (oxygen > maxOxygen) {
-            oxygen = maxOxygen;
-        }
+        if (entity.IsOwner) {
+            oxygen += amount;
+            if (oxygen > maxOxygen) {
+                oxygen = maxOxygen;
+            }
 
-        state.PlayerOxygen = oxygen;
+            state.PlayerOxygen = oxygen;
+        }
     }
 }
